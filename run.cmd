3da3d4227844896dde79c24c210f4b764fd5b866
Jrhero14 <Overload.emperor14@gmail.com>
2023-04-05 11:55:41 +0800

    fix url

--- run.cmd
+++ run.cmd
@@ -1,3 +1,3 @@
 pip install -r requirements.txt
-start cmd /k "explorer https://http://127.0.0.1:8000/ & python manage.py runserver"
+start cmd /k "explorer http://127.0.0.1:8000/ & python manage.py runserver"
 exit

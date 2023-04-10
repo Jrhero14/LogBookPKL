c209c0e02d05b6896095903eb87bbd42dafee1f4
Jrhero14 <Overload.emperor14@gmail.com>
2023-04-10 22:45:53 +0800

    Update V4.0 Deployment

--- LogBookPKL/settings.py
+++ LogBookPKL/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u^8a6uz#vwj@$#mienicn-&ans%go@uo(ml_td@)iy)9yb(+8_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

092c9ff4cc7402cfca82ba5c0cb789fc3e1845a3
Jrhero14 <Overload.emperor14@gmail.com>
2023-04-11 23:13:16 +0800

    Debug mode ON

--- LogBookPKL/settings.py
+++ LogBookPKL/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u^8a6uz#vwj@$#mienicn-&ans%go@uo(ml_td@)iy)9yb(+8_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

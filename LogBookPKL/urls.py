754c8481d57396d84d24b4f21d79feaa5d8f20cc
Jeremi Herodian Abednigo <Overload.emperor14@gmail.com>
2023-05-02 00:22:05 +0800

    Add feature login for user

--- LogBookPKL/urls.py
+++ LogBookPKL/urls.py
@@ -4,6 +4,11 @@ from User import views as userViews
 
 urlpatterns = [
     path('', userViews.indexViews),
+    path('splash/', userViews.splashviews),
+    path('auth/', userViews.loginViews),
+    path('registrasi/', userViews.register),
+    path('login/', userViews.logincekViews),
+    path('logout/', userViews.logoutViews),
     path('newLog/', userViews.newLog),
     path('editLog/', userViews.editLog),
     path('delLog/', userViews.deleteLogBook),
--- User/views.py
+++ User/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, redirect
 from .models import Log, imgDoc
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
 import datetime, os, json
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
 
 # Utils Function
 def handle_uploaded_file(f, dir):
@@ -53,8 +64,55 @@ def deleteLogBook(request):
 
 # Create your views here.
 def indexViews(request):
+    print(request.user)
+    if (str(request.user) == 'AnonymousUser'):
+        return redirect('/splash')
+
     dataLog = Log.objects.all().order_by('-id')
     contexts = {
         'logData': dataLog
     }
     return render(request, 'index.html', context=contexts)
+
+def loginViews(request):
+    if (str(request.user) != 'AnonymousUser' and request is not None):
+        return redirect('/')
+    form = RegisterForm()
+    return render(request, 'login.html', context={"form": form})
+
+def logoutViews(request):
+    if request.method == 'POST':
+        logout(request)
+    return redirect('/auth')
+
+def logincekViews(request):
+    if request.method == 'POST':
+        username = request.POST['usernameform']
+        password = request.POST['passwordform']
+        user = authenticate(username=username, password=password)
+        if (user is not None):
+            login(request, user)
+        else:
+            form = RegisterForm()
+            return render(request, 'login.html', {"form": form, "loginfalse": True})
+    else:
+        return redirect('/')
+    return redirect('/')
+
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            user = form.save()
+            login(response, user)
+            return redirect("/")
+        else:
+            form = RegisterForm()
+    else:
+        form = RegisterForm()
+        return render(response, 'login.html', context={"form": form})
+
+    return render(response, "index.html")
+
+def splashviews(request):
+    return render(request, 'splashscreen.html')
